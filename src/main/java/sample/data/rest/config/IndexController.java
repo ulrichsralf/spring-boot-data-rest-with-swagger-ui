4a8ee9f6bdfc123874e850907e15b219f826aaa5
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2017-10-21 00:05:02 +0200

    initial commit

---
+++ src/main/java/sample/data/rest/config/IndexController.java
@@ -0,0 +1,14 @@
+package sample.data.rest.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "redirect:/swagger-ui.html";
+    }
+
+}
